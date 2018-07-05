d9faa627303bfdc5921d2c6e226bec1520a4aa7a
Mahmud <omahmud@d170800.stjude.sjcrh.local>
2018-07-05 09:57:27 -0500

    code for python exercise 1

---
+++ about.me.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import sys
+
+my_name = sys.argv[1]
+favorite_color = sys.argv[2]
+favorite_activity = sys.argv[3]
+favorite_animal = sys.argv[4]
+
+print('my name is', my_name, ', my fav color is' ,favorite_color, ', my fav activity is' ,favorite_activity, ', and my fav animal is' ,favorite_animal) 
